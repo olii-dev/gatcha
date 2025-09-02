018e83563a0c3c0363d2d3ea7b489c6598472d1e
JosephK08 <josephkoboi8@gmail.com>
2025-09-02 18:41:08 +0930

    Cookie Clicker
    Cookie clicker

---
+++ game/Assets/Scripts/Gold.cs
@@ -0,0 +1,30 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    public float gold = 0;
+    public TMP_Text goldText;
+
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gold = gold + 1;
+        }
+        goldText.SetText("Gold: " + gold);   
+
+    }
+}
